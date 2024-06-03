4c417dc528c547dcb6b45490b94c528b212a8e1c
tk4500 <131721143+tk4500@users.noreply.github.com>
2024-06-03 14:41:43 -0300

    Ajustando para o dicionário

--- First_Project/First_Project/Program.cs
+++ First_Project/First_Project/Program.cs
@@ -3,7 +3,8 @@ string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 // List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso" };
 
 Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
-
+bandasRegistradas.Add("Link Park", new List<int>{ 10, 8, 6 });
+bandasRegistradas.Add("The Beatles", new List<int>());
 void ExibirLogo()
 {
     Console.WriteLine(@"
@@ -56,7 +57,7 @@ void RegistrarBanda()
     ExibirTituloDaOpcao("Registro de bandas");
     Console.Write("Digite o nome da banda que você deseja registrar: ");
     string nomeDaBanda = Console.ReadLine()!;
-    listaDasBandas.Add(nomeDaBanda);
+    bandasRegistradas.Add(nomeDaBanda, new List<int>());
     Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso");
     Thread.Sleep(2000);
     Console.Clear();
@@ -72,7 +73,7 @@ void MostrarBandasRegistradas()
     //  Console.WriteLine($"Banda: {listaDasBandas[i]}");
     // }
 
-    foreach (string banda in listaDasBandas)
+    foreach (string banda in bandasRegistradas.Keys)
     {
         Console.WriteLine($"Banda: {banda}");
     }
