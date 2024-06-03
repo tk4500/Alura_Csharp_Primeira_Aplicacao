064f0b87eec86a79c79fa6aef6fcefb6e9de8a83
tk4500 <131721143+tk4500@users.noreply.github.com>
2024-06-03 16:37:13 -0300

    Finalizando

--- First_Project/First_Project/Program.cs
+++ First_Project/First_Project/Program.cs
@@ -43,7 +43,11 @@ void ExibirOpcoesDoMenu()
             AvaliarUmaBanda();
             break;
         case 4:
+<<<<<<< Updated upstream
             MediaDaBanda();
+=======
+            ExibirMedia();
+>>>>>>> Stashed changes
             break;
         case -1:
             Console.WriteLine("Tchau tchau :)");
@@ -121,6 +125,7 @@ void AvaliarUmaBanda()
     }
 }
 
+<<<<<<< Updated upstream
 void MediaDaBanda()
 {
     Console.Clear();
@@ -132,17 +137,39 @@ void MediaDaBanda()
         List<int> notas = bandasRegistradas[nomeDaBanda];
         Console.WriteLine($"\nA média da banda {nomeDaBanda} é {notas.Average()}.");
         Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+=======
+void ExibirMedia()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Exibir média da banda");
+    Console.Write("Digite o nome da banda que deseja exibir a média: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        List<int> notasDaBanda = bandasRegistradas[nomeDaBanda];
+        Console.WriteLine($"\nA média da banda {nomeDaBanda} é {notasDaBanda.Average()}.");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal.");
+>>>>>>> Stashed changes
         Console.ReadKey();
         Console.Clear();
         ExibirOpcoesDoMenu();
     }
     else
     {
+<<<<<<< Updated upstream
         Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
         Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+=======
+        Console.WriteLine($"\n A banda {nomeDaBanda} não foi encontrada.");
+        Console.WriteLine("Digite ma tecla para voltar ao menu principal.");
+>>>>>>> Stashed changes
         Console.ReadKey();
         Console.Clear();
         ExibirOpcoesDoMenu();
     }
 }
+<<<<<<< Updated upstream
 ExibirOpcoesDoMenu();
+=======
+    ExibirOpcoesDoMenu();
+>>>>>>> Stashed changes
