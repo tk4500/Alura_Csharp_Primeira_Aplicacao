7beda11a47bc3e53aacc3da6f670d89b27d7f623
tk4500 <131721143+tk4500@users.noreply.github.com>
2024-06-03 13:04:53 -0300

    Variáveis do tipo inteiro

--- First_Project/First_Project/Program.cs
+++ First_Project/First_Project/Program.cs
@@ -21,6 +21,14 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
     Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+    if (opcaoEscolhidaNumerica == 1)
+    {
+        Console.WriteLine("Você digitou a opção " + opcaoEscolhida);
+    }
 }
 ExibirMensagemDeBoasVindas();
 ExibirOpcoesDoMenu();
